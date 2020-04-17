8b60c9c155cae455f05d84fad0484e558ac49478
danapesah <pe.dana@gmail.com>
2020-04-18 01:14:48 +0300

    commit

---
+++ build/precache-manifest.cb31dc84728c7263c919cf929d53a378.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7dec8a3501afc63597ac6d918eb7b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbbda7f78cf2bd36e06b",
+    "url": "/static/css/2.94d92c40.chunk.css"
+  },
+  {
+    "revision": "afd58e181fbd040130d0",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "bbbda7f78cf2bd36e06b",
+    "url": "/static/js/2.0c5a21fc.chunk.js"
+  },
+  {
+    "revision": "96043b6052d532ce4ff378a07ed20660",
+    "url": "/static/js/2.0c5a21fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "afd58e181fbd040130d0",
+    "url": "/static/js/main.a41d06d3.chunk.js"
+  },
+  {
+    "revision": "24dcd5cb0fc6d14b2411",
+    "url": "/static/js/runtime-main.c8e9043d.js"
+  }
+]);
