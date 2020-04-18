8db95821f13b61c32f52a69759a7c789df7de0ab
danapesah <pe.dana@gmail.com>
2020-04-18 18:34:35 +0300

    commit

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,10 @@
 const router = require('express').Router(); //rout we creating
 let User = require('../models/user.model'); // mongoose model we created
 
-
 router.route('/').get((req, res) => { //if theres a / at the end
   User.find() //mongoose methode  get a list of all the users from thr db(
     .then(users => res.json(users)) //get all the users
-    .catch(err => res.status(503).json('Error: ' + err)); //if theres error
+    .catch(err => res.status(400).json('Error: ' + err)); //if theres error
 });
 
 router.route('/add').post((req, res) => { 
